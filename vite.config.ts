3de6288ce192982e034068acc0c80a8da34298ff
Shahil00099 <your-github-shahilshrestha99@gmail.com>
2026-08-17 10:19:50 +0545

    fix: add tailwind vite plugin for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: '/vue-task-dashboard/', // <--- MUST match your GitHub repo name exactly
+  plugins: [
+    vue(),
+    tailwindcss(), // <--- Add this line
+  ],
+  base: '/vue-task-dashboard/',
 })
